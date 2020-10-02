7c3441eeec135fcf3850efd1121bf4a53b69605e
John York <johnyork807@gmail.com>
2020-10-02 12:53:12 -0400

    multiple changes to network and crypto

---
+++ 5.Crypto/Cryptography Homework 3/AES-ECB-decrypt.py
@@ -0,0 +1,8 @@
+from Crypto.Cipher import AES
+import codecs
+
+ciphertext = codecs.decode(b'5pY0H0f3NwgqAPgFjhvD0+X7V2ExYhglnLDTxcV0Lz5zrBWvwSuplFt8PR/dSN9TN1PvPSTKYJgY\nlRHoLVlVzQ==\n', 'base64')
+aes_obj = AES.new(b'This is the key!', AES.MODE_ECB)
+plaintext = aes_obj.decrypt(ciphertext)
+print(plaintext)
+
